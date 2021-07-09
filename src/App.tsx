1a15f40ed92edf0bccb89942d455fcf0353555d7
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-08 21:23:38 -0300

    refactor: estrutura inicial do App

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StatusBar, SafeAreaView } from 'react-native'
+import { ScreenProvider } from 'responsive-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import Home from './pages/Home'
+import { ThemeProvider } from './styles'
+
+const App: React.FC = () => (
+  <>
+    <StatusBar
+      backgroundColor="#fff"
+      animated
+      barStyle="dark-content"
+    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ThemeProvider>
+        <SafeAreaProvider>
+          <ScreenProvider baseFontSize={16}>
+            <Home />
+          </ScreenProvider>
+        </SafeAreaProvider>
+      </ThemeProvider>
+    </SafeAreaView>
+  </>
+)
+
+export default App
