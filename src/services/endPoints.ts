41e6c0bd00f6eec78f2c78755c11b4ea8d2858ca
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-11 21:17:30 -0300

    feat: implementação das informações dos endpoints: geocode e openWeather

---
+++ src/services/endPoints.ts
@@ -0,0 +1,10 @@
+export default {
+  geocode: {
+    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    apiKey: 'AIzaSyCStSyMkXQU-23hS61b4yuwADAHQOCyEKo'
+  },
+  openWeather: {
+    url: 'https://api.openweathermap.org/data/2.5/onecall',
+    apiKey: '4965df5842c75ded4d64bee2698f0424'
+  }
+}
