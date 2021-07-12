3c33a00437abb1e51b7d6a2c1972185e55699b01
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-11 21:16:39 -0300

    refactor: refatoração no background-color da página Home

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,5 +3,5 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   height: 100%;
   width: 100%;
-  background-color: ${({ theme }) => theme.palettes.white};
+  background-color: rgb(112, 243, 233);
 `
