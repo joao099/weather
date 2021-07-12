1698e8fe74c87fe00c7c07d163ed067f5adc12f4
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-11 21:16:02 -0300

    feat: implementação de funções para pegar a localização do usuário
    implementação da função requestLocationPermission para solicitar ao usuário permissão para usar a
    localização do dispositivo, implementação da função getOneTimeLocation para pegar a longitude e
    latitude atual do usuário, implementação da função subscribeLocation para ficar escutando a mudança
    de localização do usuário

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,133 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Platform, PermissionsAndroid } from 'react-native'
+import Geolocation from '@react-native-community/geolocation'
 
-import { Container } from './styles'
 import Header from '../../components/Header'
+import endPoints from '../../services/endPoints'
+import { Container } from './styles'
+import { useFetch } from '../../hooks/useFetch'
+import { GeolocationInterface } from '../../interfaces/GeolocationInterface'
+// import InfoWeather from '../../components/InfoWeather'
 
 const Home: React.FC = () => {
+  const [currentLongitude, setCurrentLongitude] = useState('')
+  const [currentLatitude, setCurrentLatitude] = useState('')
+  const [shouldFetch, setShouldFetch] = useState(false)
+
+  const { url, apiKey } = endPoints.geocode
+  const { data } = useFetch<GeolocationInterface>(shouldFetch ? `${url}?address=${currentLatitude},${currentLongitude}&key=${apiKey}` : null)
+
+  let titleHeader: string = ''
+  let watchID: number
+
+  useEffect(() => {
+    requestLocationPermission()
+    return () => {
+      Geolocation.clearWatch(watchID)
+    }
+  }, [])
+
+  // Se já tenho a minha longitude e minha latitude, atualize a variável de estado shouldFetch para fazer a requisição
+  useEffect(() => {
+    if (currentLongitude && currentLatitude) setShouldFetch(true)
+  }, [currentLongitude, currentLatitude])
+
+  if (data) {
+    const locationName = data.results[0].address_components.filter(item => item.types[0] === 'administrative_area_level_4')
+    titleHeader = locationName[0].long_name
+  }
+
+  // Permissão de localização do usuário
+  const requestLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      getOneTimeLocation()
+      subscribeLocation()
+    } else {
+      try {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: 'Location Access Required',
+            message: 'This App needs to Access your location',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK'
+          }
+        )
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          // Para verificar, se a permissão for concedida
+          getOneTimeLocation()
+          subscribeLocation()
+        } else {
+          console.log('falhou')
+        }
+      } catch (err) {
+        console.warn(err)
+      }
+    }
+  }
+
+  const getOneTimeLocation = () => {
+    Geolocation.getCurrentPosition(
+      // Dará a você a localização atual
+      (position) => {
+        // obtendo a longitude do local
+        const currentLongitude =
+          JSON.stringify(position.coords.longitude)
+
+        // obtendo o Latitude do local
+        const currentLatitude =
+          JSON.stringify(position.coords.latitude)
+
+        // Setando o estado de longitude
+        setCurrentLongitude(currentLongitude)
+
+        // Setando o estado de Latitude
+        setCurrentLatitude(currentLatitude)
+      },
+      (error) => {
+        console.log('entrou erro', error)
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 30000,
+        maximumAge: 1000
+      }
+    )
+  }
+
+  const subscribeLocation = () => {
+    watchID = Geolocation.watchPosition(
+      (position) => {
+        // obtendo a longitude do local
+        const currentLongitude =
+          JSON.stringify(position.coords.longitude)
+
+        // obtendo o Latitude do local
+        const currentLatitude =
+          JSON.stringify(position.coords.latitude)
+
+        // Setando o estado de longitude
+        setCurrentLongitude(currentLongitude)
+
+        // Setando o estado de Latitude
+        setCurrentLatitude(currentLatitude)
+      },
+      (error) => {
+        console.log('error', error)
+      },
+      {
+        enableHighAccuracy: true,
+        maximumAge: 1000
+      }
+    )
+  }
+
   return (
     <Container>
-      <Header />
+      <Header titleHeader={titleHeader} />
+      {/* <InfoWeather /> */}
 
-      {/* <WhiteContainer>
-        <Title></Title>
-        <Description></Description>
-        <WeatherImage />
-      </WhiteContainer>
-
-      <ContentContainer>
+      {/* <ContentContainer>
         <HorizontalContainer>
           <MoistureContainer>
             <MoistureImage />
