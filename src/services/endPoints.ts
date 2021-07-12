4368ccf1fd5d53564711532e677b4cbd63f39624
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-12 08:40:31 -0300

    refactor: refatoração nos endpoints

--- src/services/endPoints.ts
+++ src/services/endPoints.ts
@@ -1,11 +1,11 @@
 export default {
   geocode: {
-    url: 'https://maps.googleapis.com/maps/api/geocode/json',
+    url: (lat: string, long: string) => `https://maps.googleapis.com/maps/api/geocode/json?address=${lat},${long}&key=AIzaSyCStSyMkXQU-23hS61b4yuwADAHQOCyEKo`,
     apiKey: 'AIzaSyCStSyMkXQU-23hS61b4yuwADAHQOCyEKo'
   },
   openWeather: {
-    url: 'https://api.openweathermap.org/data/2.5/onecall',
-    urlImage: 'http://openweathermap.org/img/wn',
+    url: (lat: string, long: string) => `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&lang=pt_br&units=metric&appid=4965df5842c75ded4d64bee2698f0424`,
+    urlImage: (nameImage: string) => `http://openweathermap.org/img/wn/${nameImage}@2x.png`,
     apiKey: '4965df5842c75ded4d64bee2698f0424'
   }
 }
