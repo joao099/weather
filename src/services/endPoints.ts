0cd9720e403492fae0c06421911e5614951fafb5
João Vitor da Cruz <ext-joao.nunes@bbts.com.br>
2021-07-12 03:38:45 -0300

    refactor: implementação do end point de imagem do openweather

--- src/services/endPoints.ts
+++ src/services/endPoints.ts
@@ -5,6 +5,7 @@ export default {
   },
   openWeather: {
     url: 'https://api.openweathermap.org/data/2.5/onecall',
+    urlImage: 'http://openweathermap.org/img/wn',
     apiKey: '4965df5842c75ded4d64bee2698f0424'
   }
 }
